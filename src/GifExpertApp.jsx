fbcf1fc7c2f2e2dc5b86fc76a3ecb1ac4e9e2f3a
Miguel Angel Rios Yañez <Miguel.a.r.y2310@hotmail.com>
2021-03-06 13:44:45 -0600

    Aprendiendo a usar el useState para agregar una categoria en bruto

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const GifExpertApp = () => {
+
+  // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+  const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball'])
+
+  const handleAdd = () => {
+    // setCategories([...categories, 'Superman', 'Batman', 'Spiderman', 'HunterXHunter']);
+    setCategories( category => [...category, 'Superman', 'Batman', 'Spiderman', 'HunterXHunter']);
+  };
 
   return(
     <>
       <h2>GifExpertApp</h2>
       <hr />
+
+      <button onClick={handleAdd}>Agregar</button>
+
+      <ol>
+        { categories.map( (category) => {
+          return <li key={category}>{category}</li>
+        }) }
+      </ol>
     </>
   )
 };
